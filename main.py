6b558438c12bbc82061ab7a9cc5957c0e23ebaca
oooazimut <159106807+oooazimut@users.noreply.github.com>
2024-06-20 11:27:14 +0300

    test

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton('Web App', web_app=WebAppInfo(url="https://2ch.hk/")))
+    markup.add(types.InlineKeyboardButton('Web App', web_app=WebAppInfo(url="https://github.com/SwEEtL0Li/test/test.html")))
     await message.reply("Hello!", reply_markup=markup)
 
 executor.start_polling(dp)
