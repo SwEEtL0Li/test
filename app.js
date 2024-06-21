b9e5905671b11b2e3ca164783000ac71f787a6d3
SwEEtL0Li <99595122+SwEEtL0Li@users.noreply.github.com>
2024-06-21 09:00:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -69,18 +69,6 @@ btn5.addEventListener("click", function(){
 	}
 });
 
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
-});
-
-
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
